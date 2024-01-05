de3ff2f9be3c33e5f507fe68709953cda595d1f1
InnaYal <147692091+InnaYal@users.noreply.github.com>
2024-01-05 19:05:04 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://fe569e18-d1d3-41bf-950e-454df2d9da20.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,12 @@
+headers = {
+    "Content-Type": "application/json"
+}
+order_payload = {"firstName": "Inna",
+                 "lastName": "Yaltonskaya",
+                 "address": "Moscow, 155 apt.",
+                 "metroStation": 2,
+                 "phone": "+7 999 99 99",
+                 "rentTime": 4,
+                 "deliveryDate": "2020-01-19",
+                 "comment": "Тест для Диплома. Ялтонская.",
+                 "color": ["BLACK"]}
